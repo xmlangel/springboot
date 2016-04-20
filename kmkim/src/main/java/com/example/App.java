dc7f0e3c2de1aeced7579641291796a45822ce04
Kwangmyung <kmkim@whatap.io>
2016-04-21 07:25:22 +0900

    스프링로디드

--- kmkim/src/main/java/com/example/App.java
+++ kmkim/src/main/java/com/example/App.java
@@ -1,13 +1,20 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration ;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // (1)
+@EnableAutoConfiguration // (2)
+public class App {
+
+    @RequestMapping( "/") // (3)
+    String home() {
+        return "Hello World! Spring" ; // (4)
+    }
+
+    public static void main(String[] args) {
+        SpringApplication. run(App.class, args); // (5)
     }
 }
